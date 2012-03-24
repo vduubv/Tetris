a02eda2cc5af485fff1a30833f55a72715b8aa20
seunghyun <seunghyun@organiza-60925e>
2012-03-24 16:56:28 +0900

    올린다

--- src/Tetris.java
+++ src/Tetris.java
@@ -35,7 +35,7 @@ public class Tetris extends Applet implements Runnable{
 		offG.setColor(Color.white);
 		offG.fillRect(0, 0, 192, 192);
 		
-		turnAudio = getAudioClip(getCodeBase(), "turn.au");
+		turnAudio = getAudioClip(getCodeBase(), "turn.au"); //사운드 로드
 		deleteAudio = getAudioClip(getCodeBase(), "delete.au");
 		gameOverAudio = getAudioClip(getCodeBase(), "gameover.au");
 		
@@ -48,21 +48,137 @@ public class Tetris extends Applet implements Runnable{
 		blockY = new int[4];
 		blockPos = 0;
 		
-		r = new Random();
+		r = new Random(); // 처음에 내보낼 블록의 종류를 선택(0~7)
 		blockType = Math.abs(r.nextInt()%7);
-		setBlockXY(blockType);
+		setBlockXY(blockType); //블록에 종류에 따라 블록의 각 칸을 구성
 		
-		drawBlock();
-		drawMap();
-		drawGrid();
+		drawBlock(); // 떨어지는 블록그리기
+		drawMap(); // 화면 그리기
+		drawGrid(); // 모눈 선(격자) 그리기
 		
 		score = 0;
 		delayTime = 1000;
 		runGame = true;
 		
-		this.requestFocus();
+		this.requestFocus(); //키보드 포커스 요청
 		
 		addKeyListener(new MyKeyHandler());
 	}
+	
+	public void setColorType(){ //게임에서 사용하는 7종류의 색을 미리 지정
+		colorType[0]= new Color(65, 228, 82); //ㅁ 모양 색상
+		colorType[1]= new Color(58, 98, 235); //ㅓ 모양 색상
+		colorType[2]= new Color(128, 0, 64); //ㄱ 모양 색상
+		colorType[3]= new Color(255, 35, 31); //ㄱ 반대 모양 색상
+		colorType[4]= new Color(68, 17, 111); //Z 모양 색상
+		colorType[5]= new Color(246, 118, 57); //Z 반대 모양 색상
+		colorType[6]= new Color(224, 134, 4); //ㅣ 모양 색상
+	}
+	
+	public void setBlockXY(int type){ //type에 따라 블록을 구성
+		switch(type){
+		case 0 : //ㅁ 모양
+			blockX[0]=5; blockY[0]=0;
+			blockX[1]=6; blockY[1]=0;
+			blockX[2]=5; blockY[2]=1;
+			blockX[3]=6; blockY[3]=1;
+			break;
+		case 1 : //ㅓ 모양
+			blockX[0]=6; blockY[0]=0;
+			blockX[1]=5; blockY[1]=1;
+			blockX[2]=6; blockY[2]=1;
+			blockX[3]=7; blockY[3]=1;
+			break;
+		case 2 : //ㄱ 모양
+			blockX[0]=7; blockY[0]=0;
+			blockX[1]=5; blockY[1]=1;
+			blockX[2]=6; blockY[2]=1;
+			blockX[3]=7; blockY[3]=1;
+			break;
+		case 3 : //ㄱ 반대 모양
+			blockX[0]=5; blockY[0]=0;
+			blockX[1]=5; blockY[1]=1;
+			blockX[2]=6; blockY[2]=1;
+			blockX[3]=7; blockY[3]=1;
+			break;
+		case 4 : // Z 모양
+			blockX[0]=5; blockY[0]=0;
+			blockX[1]=5; blockY[1]=1;
+			blockX[2]=6; blockY[2]=1;
+			blockX[3]=6; blockY[3]=2;
+			break;
+		case 5 : // Z 반대 모양
+			blockX[0]=6; blockY[0]=0;
+			blockX[1]=5; blockY[1]=1;
+			blockX[2]=6; blockY[2]=1;
+			blockX[3]=5; blockY[3]=2;
+			break;
+		case 6 : // ㅣ  모양
+			blockX[0]=4; blockY[0]=0;
+			blockX[1]=5; blockY[1]=0;
+			blockX[2]=6; blockY[2]=0;
+			blockX[3]=7; blockY[3]=0;
+			break;
+		}
+	}
+	
+	public void start(){
+		if(clock ==null){
+			clock = new Thread(this);
+			clock.start(); //시계 시작
+		}
+	}
+	
+	public void paint(Graphics g){
+		//가상화면을 실제화면에 출력
+		g.drawImage(off, 0,0,this);
+	}
+	
+	public void update(Graphics g){
+		paint(g);
+	}
+	
+	public void run(){
+		while(true){
+			try{
+				clock.sleep(delayTime);
+			}catch(InterruptedException ie){}
+			
+			dropBlock(); //블록을 1행 떨어뜨림
+			if(runGame){
+				drawBlock(); //떨어지는 블록을 그리기
+				drawMap(); //화면 그리기
+				drawGrid(); //모눈 선(격자)그리기
+			}else{
+				drawScore(); //성적 출력
+			}
+			repaint(); //paint()호출
+		}
+	}
+	
+	public void drawScore(){ //성적 출력
+		offG.setColor(Color.white);
+		offG.fillRect(35, 120, 110, 70);
+		offG.setColor(Color.black);
+		offG.drawRect(40, 125, 100, 60);
+		offG.setColor(Color.red);
+		offG.drawString("Game over !", 56, 150);
+		offG.setColor(Color.blue);
+		offG.drawString("Score : "+score, 56, 170);
+	}
+	
+	public void dropBlock(){ // 블록을 1행 떨어뜨림
+		removeBlock(); //현재 블록을 제거
+		if(checkDrop()){ //블록을 1행 아래로 이동 가능한 지 확인
+			for(int i=0; i<4; i++){
+				blockY[i]=blockY[i]+1;
+			}
+		}
+		else{
+			drawBlock(); //블록을 그리기
+			nextBlock(); //다음 블록을 결정
+		}
+	}
+	
 	
 }
